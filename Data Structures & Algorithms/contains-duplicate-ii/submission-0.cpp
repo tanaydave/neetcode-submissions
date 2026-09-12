38947a0192fce0e31b536c1baa38a0953eca5457
tanaydave <114003362+tanaydave@users.noreply.github.com>
2026-09-11 20:00:21 -0400

    Add: contains-duplicate-ii - submission-0

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int,int>mpp;
+        for(int i = 0; i < nums.size();i++)
+        {
+            if(mpp.count(nums[i]))
+            {
+                if(abs(i - mpp[nums[i]]) <= k) return true;
+                else mpp[nums[i]] = i;
+            } 
+            else mpp[nums[i]] = i;
+
+            cout<<i + mpp[nums[i]];
+        }
+        return false;
+        
+    }
+};
